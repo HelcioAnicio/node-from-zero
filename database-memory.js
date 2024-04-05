f60ef8e98dcb0e31606dc2d6e4653f972541c3d0
Helcio <helcioanicio19@gmail.com>
2024-04-04 23:04:18 -0300

    Chore: Created file routes.http to do resquests on http, and wait responses.

--- database-memory.js
+++ database-memory.js
@@ -1,3 +1,22 @@
-export class DatabaseMemory {
+import { randomUUID } from "node:crypto"
 
+export class DatabaseMemory {
+ #videos = new Map()
+
+  list() {
+    return Array.from(this.#videos.values())
+  }
+
+  create(video) {
+    const videoId = randomUUID()
+    this.#videos.set(videoId, video)
+  }
+
+  update(id, video) {
+    this.#videos.set(id, video)
+  }
+
+  delete(id, video) {
+    this.#videos.delete(id)
+  }
 }
