e6ac0083584e15a450178d8d0c33d27bf9075c80
Helcio <helcioanicio19@gmail.com>
2024-04-08 23:29:48 -0300

    Chore: Improve the routes and server, and using async and await on server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import { fastify } from "fastify";
-import { DatabaseMemory } from "./database-memory.js";
+// import { DatabaseMemory } from "./database-memory.js";
+import { DatabasePostgres } from "./database-postgres.js";
 
 const server = fastify()
-const database = new DatabaseMemory()
+// const database = new DatabaseMemory()
+const database = new DatabasePostgres()
 
-server.post('/videos', (request, reply) => {
+server.post('/videos', async (request, reply) => {
   const { title, description, duration } = request.body
   
-  database.create({
+  await database.create({
     title,
     description,
     duration,
@@ -17,19 +19,19 @@ server.post('/videos', (request, reply) => {
 })
 
 
-server.get('/videos', (request) => {
+server.get('/videos', async (request) => {
   const search = request.query.search
-  console.log(search)
-  const videos = database.list(search)
+
+  const videos = await database.list(search)
   return videos
 })
 
 
-server.put('/videos/:id', (request, reply) => {
+server.put('/videos/:id',async (request, reply) => {
   const videoId = request.params.id
   const { title, description, duration } = request.body
 
-  database.update(videoId, {
+  await database.update(videoId, {
     title,
     description,
     duration,
@@ -39,10 +41,10 @@ server.put('/videos/:id', (request, reply) => {
 })
 
 
-server.delete('/videos/:id', (request, reply) => {
+server.delete('/videos/:id', async (request, reply) => {
   const videoId = request.params.id
 
-  database.delete(videoId)
+  await database.delete(videoId)
 
   return reply.status(204).send()
 })
@@ -50,5 +52,5 @@ server.delete('/videos/:id', (request, reply) => {
 
 
 server.listen({
-  port: 3333
+  port: process.env.PORT ?? 3333,
 })
