f67ee4b134e3b4a0fee33d64e1fea488bb35df13
Helcio <helcioanicio19@gmail.com>
2024-04-08 23:26:51 -0300

    Chore: Created tables and seted functions and methods to require the dates and informations.

--- db.js
+++ db.js
@@ -1,26 +1,11 @@
-// const { default: postgres } = await import('postgres')
 import postgres from 'postgres'
 import 'dotenv/config'
-// import { config } from 'dotenv';
-// config();
-
-// const postgres = require('postgres');
-// require('dotenv').config();
-
 
 let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-const sql = postgres({
-  host: PGHOST,
-  database: PGDATABASE,
-  username: PGUSER,
-  password: PGPASSWORD,
-  port: 3333,
-  ssl: 'require',
-  connection: {
-    options: `project=${ENDPOINT_ID}`,
-  },
-});
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project=${ENDPOINT_ID}`;
 
 
-export default sql
+
+export const sql = postgres(URL, { ssl: 'require'});
+
